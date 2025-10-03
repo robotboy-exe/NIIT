a776b5d3df467eff288fc1e2073af70755a4aaed
robotboy <odeyovwiosamuel@gmail.com>
2025-10-03 05:10:24 +0100

    update

---
+++ Project/app.js
@@ -0,0 +1,5 @@
+const cardTemplate = document.querySelector("#card-template")
+const carousel = document.querySelector(".carousel")
+const newReleases = document.querySelector("#newReleases")
+
+const 
