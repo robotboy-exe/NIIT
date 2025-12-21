d52bca87b4b88c0f384b15d930761ef6a831b157
Samuel <odeyovwiosamuel@gmail.com>
2025-12-21 17:02:29 +0100

    indentation

--- Project/app.js
+++ Project/app.js
@@ -1,5 +1,3 @@
 const cardTemplate = document.querySelector("#card-template")
 const carousel = document.querySelector(".carousel")
 const newReleases = document.querySelector("#newReleases")
-
-const 
--- Project/weather-login/app.js
+++ Project/weather-login/app.js
@@ -1,24 +1,24 @@
-const toggleButtons = document.querySelectorAll('button[data-target]');
+const toggleButtons = document.querySelectorAll("button[data-target]");
 
-toggleButtons.forEach(btn => {
-  btn.addEventListener('click', () => {
+toggleButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
     const input = document.querySelector(btn.dataset.target);
-    
-    const isHidden = input.type === 'password';
-    
+
+    const isHidden = input.type === "password";
+
     if (!input) return;
-    
+
     if (isHidden) {
-      input.type='text';
-      btn.textContent='HIDE';
+      input.type = "text";
+      btn.textContent = "HIDE";
     } else {
-      input.type='password';
-      btn.textContent='SHOW';
+      input.type = "password";
+      btn.textContent = "SHOW";
     }
   });
 });
 
-console.dir(toggleButtons[0])
-console.log(toggleButtons[0])
-console.log(toggleButtons[0].dataset.target)
-console.log(document.querySelector('input#password'))
+console.dir(toggleButtons[0]);
+console.log(toggleButtons[0]);
+console.log(toggleButtons[0].dataset.target);
+console.log(document.querySelector("input#password"));
